2d3ef4c4d61be52ab1fe1c0e38beae2b6d5e71b4
NightW0lfff <philong170101@gmail.com>
2025-12-26 01:07:06 +1100

    init: Setup basic features for server side.

---
+++ server/src/app.ts
@@ -0,0 +1,40 @@
+import express, {
+	type Application,
+	type Request,
+	type Response,
+	type NextFunction,
+} from "express";
+import cors from "cors";
+import morgan from "morgan";
+// import routes from "./routes/routes.js";
+
+const app: Application = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Routes
+
+// Mount all route under /api
+
+// Health check
+app.use("/health", (_req: Request, res: Response, _next: NextFunction) => {
+	return res.status(200).json({ msg: "OK" });
+});
+
+// 404
+app.use((_req: Request, res: Response) => {
+	res.status(404).json({ err: "Not Found" });
+});
+
+// error handler
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+	console.error(err.message);
+	res
+		.status(err.status || 500)
+		.json({ error: err.message || "Internal Error" });
+});
+
+export default app;
